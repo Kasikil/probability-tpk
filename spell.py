edec2d77ada92b65992739a0f497c26e8401879e
Clayton Summers <clayton.wwjd@gmail.com>
2026-04-25 20:59:20 -0500

    Working on #20 by adding in a lot of cantrips and their scaffolding

--- spell.py
+++ spell.py
@@ -6,13 +6,38 @@ class Spell:
     def __init__(self, data):
         self.name = data.get("name", "Unknown Spell")
         self.level = data.get("level", 1)
-        self.damage_type = data.get("damage_type", "Psychic")
-        self.damage_die = data.get("damage_die", 6)
+        self.damage_type = data.get("damage_type", None)
+        self.damage_die = data.get("damage_die", 0)
         self.die_count = data.get("die_count", 1)
         self.additional_damage = data.get("additional_damage", 0)
-        self.spell_type = data.get("spell_type", "Damage")
-        self.heal_die = data.get("heal_die", 8)
+        self.spell_type = data.get("spell_type", "Unknown")
+        self.heal_die = data.get("heal_die", 0)
         self.heal_die_count = data.get("heal_die_count", 1)
+        self.components = data.get("components", []) #TODO: Implement
+        self.casting_time = data.get("casting_time", "1 Action")  #TODO: Implement
+        self.range = data.get("range", 0) #TODO: Implement
+        self.duration = data.get("duration", 0) # Rounds  #TODO: Implement
+        self.is_melee_spell_attack = data.get("is_melee_spell_attack", False) #TODO: Implement
+        self.requires_weapon_attack = data.get("requires_weapon_attack", True) #TODO: Implement
+        self.hitpoint_block = data.get("hitpoint_block", "Next Turn Start") #TODO: Implement
+        self.gives_disadvantage = data.get("gives_disadvantage", "Undead") #TODO: Implement
+        self.upcast_type = data.get("upcast_type", "character_level") #TODO: Implement
+        self.upcast = data.get("upcast", {}) #TODO: Implement
+        self.multi_attack = data.get("multi-attack", False) #TODO: Implement
+        self.target_next_saving_throw_penalty = data.get("target_next_saving_throw_penalty", 0) #TODO: Implement
+        self.speed_reduction = data.get("speed_reduction", 0) #TODO: Implement
+        self.speed_reduction_duration = data.get("speed_reduction_duration", 0) #TODO: Implement - Measured to the start of next turn
+        self.drop_prone = data.get("drop_prone", False) #TODO: Implement
+        self.resistances_granted = data.get("resistances_granted", []) #TODO: Implement
+        self.ends = data.get("ends", None) #TODO: Implement
+        self.persistant_hazard = data.get("persistant_hazard", False) #TODO: Implement
+        self.damage_per_round = data.get("damage_per_round", False) #TODO: Implement
+        self.concentration = data.get("concentration", False) #TODO: Implement
+        self.different_damage_on_secondary_target = data.get("different_damage_on_secondary_target", False) #TODO: Implement
+        self.secondary_target_damage_die = data.get("secondary_target_damage_die", "0") #TODO: Implement
+        self.secondary_target_die_count = data.get("secondary_target_die_count", 0) #TODO: Implement
+        self.secondary_target_damage_type = data.get("secondary_target_damage_type", None) #TODO: Implement
+        self.secondary_target_additional_damage = data.get("secondary_target_additional_damage", "0") #TODO: Implement
 
     def calculate_single_target_damage(self, modifier, critical_mod=1):
         """
